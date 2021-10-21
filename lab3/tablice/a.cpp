7736183053081b2e5838ced0badff521fcd4998c
madixo <i@madix.dev>
2021-10-21 20:25:00 +0200

    Update tablice

--- lab3/tablice/a.cpp
+++ lab3/tablice/a.cpp
@@ -2,22 +2,71 @@
 #include <random>
 using namespace std;
 
-int random(int a, int b) {
+// Zadanie 1
+int random(int min, int max) {
 
     static random_device dev;
     static mt19937 rng(dev());
 
-    uniform_int_distribution<mt19937::result_type> dist(a, b);
+    uniform_int_distribution<mt19937::result_type> dist(min, max);
 
     return dist(rng);
 
 }
 
+// Zadanie 2
+void fill_random(int tab[], int n, int min, int max) {
+
+    for(int i = 0; i < n; i++) {
+
+        tab[i] = random(min, max);
+
+    }
+
+}
+
+// Zadanie 3
+void reverse(int tab[], int n) {
+
+    for(int i = 0, temp; i < n / 2; i++) {
+        
+        temp = tab[i];
+        tab[i] = tab[n - i - 1];
+        tab[n - i - 1] = temp;
+
+    }
+
+}
+
+void show(int tab[], int n) {
+
+    cout << '[';
+
+    for(int i = 0; i < n; i++) cout << tab[i] << ", ";
+
+    cout << "\b\b]\n";
+
+}
+
 int main() {
 
-    int a = 5, b = 10;
+    int n;
 
-    cout << random( 1, 10);
+    cout << "Podaj ilosc elementow: ";
+
+    cin >> n;
+
+    int *tab = new int[n];
+
+    fill_random(tab, n, 0, 100);
+
+    show(tab, n);
+
+    reverse(tab, n);
+
+    show(tab, n);
+
+    delete[] tab;
 
     return 0;
 
